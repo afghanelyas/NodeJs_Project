a59956196669452eb138ade9ddeee16952135890
afghanelyas <afghan.elyas1@gmail.com>
2023-02-25 21:56:26 +0430

    error validation

--- model/things.js
+++ model/things.js
@@ -35,7 +35,7 @@ const Course = mongoose.model("Course", courseSchema);
 async function createCourse() {
   const course = new Course({
     name: "Python Course",
-    category: "web",
+    category:" ",
     author: "Elyas Afghan",
     tags: [],
     isPublished: true,
@@ -45,7 +45,7 @@ async function createCourse() {
     const result = await course.save();
     console.log(result);
   } catch (ex) {
-    console.log(ex.message);
+    for (field in ex.errors) console.log(ex.errors[field].message);
   }
 }
 createCourse();
